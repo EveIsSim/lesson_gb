d1818afac2cbd4862f1c52fd5480e2c057c2955c
EveIsSim <eve.is.sim@gmail.com>
2021-03-08 21:32:58 +0300

    lesson_8

---
+++ src/com/lesson_gb/CloseActionListener.java
@@ -0,0 +1,13 @@
+package com.lesson_gb;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CloseActionListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        System.exit(0);
+    }
+}
